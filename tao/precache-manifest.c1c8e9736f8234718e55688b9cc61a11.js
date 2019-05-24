b4737140ddb7fea5764403cf1bb19bf5fc149b6e
John Ttubert <jtubert@hotmail.com>
2019-05-24 16:12:26 -0700

    adding tao files

---
+++ tao/precache-manifest.c1c8e9736f8234718e55688b9cc61a11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bad88279048f967992b78e844daa23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb21f5d135155e81d438",
+    "url": "/static/css/main.439365d0.chunk.css"
+  },
+  {
+    "revision": "2c2206fb241ec359d03b",
+    "url": "/static/js/2.7cdf6bb3.chunk.js"
+  },
+  {
+    "revision": "cb21f5d135155e81d438",
+    "url": "/static/js/main.e85f2cb0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
