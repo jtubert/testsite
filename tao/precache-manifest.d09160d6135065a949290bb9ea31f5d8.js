c17d5902deb2c53b019449fd00906431dd523544
John Ttubert <jtubert@hotmail.com>
2019-06-12 12:55:37 -0700

    new tao site

---
+++ tao/precache-manifest.d09160d6135065a949290bb9ea31f5d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b862d2fcc2dd1784dd9c74c145369e1f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bde8ebbeb64d1accca32",
+    "url": "./static/css/main.439365d0.chunk.css"
+  },
+  {
+    "revision": "4ea90d6208f745ef0aab",
+    "url": "./static/js/2.eb350036.chunk.js"
+  },
+  {
+    "revision": "bde8ebbeb64d1accca32",
+    "url": "./static/js/main.12a38bf5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- tao/service-worker.js
+++ tao/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ae7db61a941443b90445eb31c4a2b0c.js"
+  "./precache-manifest.d09160d6135065a949290bb9ea31f5d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
